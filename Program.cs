156bec2f187aaca7be3d312479c4e2d406233e80
boodscode237 <boodscode@gmail.com>
2022-11-08 16:39:41 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // string firstName;
+            Console.Write("What is your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name: ");
+            string lastName = Console.ReadLine();
+/*
+            if (firstName != null && firstName.ToLower() == "john")
+            {
+                Console.WriteLine($"Welcome {firstName} {lastName}");
+            }
+            else if (firstName != null && firstName.ToLower() == "doe")
+            {
+                Console.WriteLine($"Welcome {firstName} {lastName}");
+            }
+            else
+            {
+                if (firstName != null) Console.WriteLine($"Sorry {firstName.ToUpper()} not found");
+            }
+            
+*/
+            if (lastName != null 
+                && firstName != null 
+                && (firstName.ToLower() == "john" 
+                    && lastName.ToLower() == "doe" 
+                    || firstName.ToLower() == "elon"
+                    && lastName.ToLower() == "musk"))
+            {
+                Console.WriteLine($"Hello {firstName} {lastName}");
+            }
+            else
+            {
+                Console.WriteLine($"Not in DB Mr {lastName} {firstName}");
+            }
+
+            switch (firstName?.ToLower())
+            {
+                case "elon":
+                    Console.WriteLine("Hello Expert");
+                    break;
+                case "john":
+                    Console.WriteLine("Hello Prof");
+                    break;
+                default:
+                    Console.WriteLine("Hello student");
+                    break;
+            }
+            Console.WriteLine("All done");
+            Console.ReadLine();
+        }
+    }
+}
